3ca3eaf5770e425484908f2be7cbf66eae24fa37
leo <wangliyingwad@hotmai.com>
2016-06-02 17:26:59 +0800

    环形

--- library_roundimage/src/main/java/com/leo/library_roudimage/ShaderRoundImageView.java
+++ library_roundimage/src/main/java/com/leo/library_roudimage/ShaderRoundImageView.java
@@ -5,10 +5,14 @@ import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.BitmapShader;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.graphics.RectF;
 import android.graphics.Shader;
+import android.graphics.SweepGradient;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
@@ -25,11 +29,16 @@ public class ShaderRoundImageView extends ImageView {
     float circleRadius;
     private static final int TYPE_CIRCLE = 0;
     private static final int TYPE_ROUND = 1;
+    private static final int TYPE_LOOP = 2;
     int viewWidth;
 
     Matrix mMatrix = new Matrix();
     Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    Paint mLoopPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
     BitmapShader mBitmapShader;
+    SweepGradient mSweepGradient;
+    int mBorderColor;
+    int mBorderWidth;
 
     public ShaderRoundImageView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -39,16 +48,20 @@ public class ShaderRoundImageView extends ImageView {
     private void init(Context context, AttributeSet attrs) {
         TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.RoundImageView);
         type = ta.getInt(R.styleable.RoundImageView_type, TYPE_CIRCLE);
-        roundRadius = ta.getDimensionPixelSize(R.styleable.RoundImageView_cornerRadius, (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 10, getResources().getDisplayMetrics()));
+        roundRadius = ta.getDimensionPixelSize(R.styleable.RoundImageView_cornerRadius,
+                (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 10, getResources().getDisplayMetrics()));
+        mBorderColor = ta.getColor(R.styleable.RoundImageView_borderColor, Color.WHITE);
+        mBorderWidth = ta.getDimensionPixelOffset(R.styleable.RoundImageView_borderWidth, (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                2, getResources().getDisplayMetrics()));
         ta.recycle();
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        if (type == TYPE_CIRCLE) {
+        if (type == TYPE_CIRCLE || type == TYPE_LOOP) {
             viewWidth = Math.min(getMeasuredWidth(), getMeasuredHeight());
-            circleRadius = viewWidth / 2;
+            circleRadius = viewWidth / 2 - mBorderWidth;
             setMeasuredDimension(viewWidth, viewWidth);
         }
     }
@@ -59,6 +72,12 @@ public class ShaderRoundImageView extends ImageView {
         setUpShader();
         if (type == TYPE_CIRCLE) {
             canvas.drawCircle(circleRadius, circleRadius, circleRadius, mPaint);
+        } else if (type == TYPE_LOOP) {
+            mLoopPaint.setShader(mSweepGradient);
+            canvas.drawCircle(viewWidth / 2, viewWidth / 2, viewWidth / 2, mLoopPaint);
+            mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_OVER));
+            canvas.drawCircle(viewWidth / 2, viewWidth / 2, circleRadius, mPaint);
+            mPaint.setXfermode(null);
         } else {
             canvas.drawRoundRect(roundRec, roundRadius, roundRadius, mPaint);
         }
@@ -85,6 +104,10 @@ public class ShaderRoundImageView extends ImageView {
         if (type == TYPE_CIRCLE) {
             int minWidth = Math.min(bitmap.getWidth(), bitmap.getHeight());
             scale = viewWidth * 1.0f / minWidth;
+        } else if (type == TYPE_LOOP) {
+            int minWidth = Math.min(bitmap.getWidth(), bitmap.getHeight());
+            scale = viewWidth * 1.0f / minWidth;
+            mSweepGradient = new SweepGradient(viewWidth / 2, viewWidth / 2, mBorderColor, mBorderColor);
         } else {
             scale = Math.max(getWidth() * 1.0f / bitmap.getWidth(), getHeight() * 1.0f / bitmap.getHeight());
         }
